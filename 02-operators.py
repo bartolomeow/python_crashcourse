e2cb237f0fac3398c59686b7ee43c5c6c497281f
bartolomeow <carlospaque95@gmail.com>
2023-04-07 19:03:06 +0200

    first operator

---
+++ 02-operators.py
@@ -0,0 +1,19 @@
+# Operadores test py
+
+x = 3
+print(x) # 3
+
+# Sumas y restas
+x += 5
+print(x) # 8
+
+# Multiplicaciones y divisiones
+y = x
+y *= 2
+print(y, type(y)) # 16
+y /= 2
+print(y, type(y))
+y %= 3
+print(y, type(y))
+y //= 1
+print(y, type(y))
