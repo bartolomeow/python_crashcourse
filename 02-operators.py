5c1d76409427c59bf389cb0fda719021e51f7d1e
bartolomeow <carlospaque95@gmail.com>
2023-04-09 11:41:27 +0200

    operators pw and concat diff types

--- 02-operators.py
+++ 02-operators.py
@@ -10,10 +10,15 @@ print(x) # 8
 # Multiplicaciones y divisiones
 y = x
 y *= 2
-print(y, type(y)) # 16
-y /= 2
-print(y, type(y))
+print(y, type(y)) # 16 int
+y /= 3
+print(y, type(y)) # 5.333 float
 y %= 3
-print(y, type(y))
-y //= 1
-print(y, type(y))
+print(y, type(y)) # 2.333 float
+print(10 // 3, type(10 // 3)) # floor division, no tiene en cuenta el resto
+
+# Potencias
+print(2 ** 3) # 8 int
+
+#print("Hola " + 5) error! No se pueden mezclar tipos
+print("Hola " + str(5)) # error! No se pueden mezclar tipos
